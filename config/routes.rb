78bb2879fc7d5f62fe8568f82f9f798d108baa6e
github username <github email address>
2017-05-25 14:19:42 -0400

    namespacing and gems updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :v1 do
+    resources :schools, only: :index do
+      resources :classrooms, only: :index do
+        resources :students, only: :show do
+        end
+      end
+    end 
+  end
+
 end
