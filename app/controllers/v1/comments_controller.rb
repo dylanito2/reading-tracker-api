3e91bc0bd27488fd813dc9c357925d66b67a4b41
github username <github email address>
2017-05-25 18:08:56 -0400

    added comment

--- app/controllers/v1/comments_controller.rb
+++ app/controllers/v1/comments_controller.rb
@@ -5,7 +5,7 @@ class V1::CommentsController < ApplicationController
     if student
       comment = Comment.new(comment_params)
       if comment.save
-        render json: "Successfully saved post", status: 200
+        render json: student, serializer: V1::StudentSerializer
       else
         render json: "Unable to save post", status: 401
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525162005) do
-
+ActiveRecord::Schema.define(version: 20170525200057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
