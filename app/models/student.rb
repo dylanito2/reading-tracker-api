6288a77feb249267b44d78d713d304a0b1316547
github username <github email address>
2017-05-26 12:01:50 -0400

    back-end ready to go for vis

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,18 @@ class Student < ApplicationRecord
   belongs_to :classroom
   has_many :comments
   has_many :reading_levels
+
+  SCORING_ARRAY = [*"1".."9"] + [*'a'..'z']
+
+  def calculate_reading_level_objects
+    self.reading_levels.map do |score|
+      rl[display] = score
+      rl[value] = generate_numeric_value(score)
+    end
+  end
+
+  def generate_numeric_value(score)
+    SCORING_ARRAY.indexOf(score) + 1
+  end
+
 end
