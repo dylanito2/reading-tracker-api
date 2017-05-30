28af1e5cc24ee708a6a9a85f854bad6a7a63b082
github username <github email address>
2017-05-30 19:40:11 -0400

    done

--- app/serializers/v1/conference_serializer.rb
+++ app/serializers/v1/conference_serializer.rb
@@ -1,5 +1,10 @@
 class V1::ConferenceSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :date
   has_many :comments, serializer: V1::CommentSerializer
   has_one :reading_level, serializer: V1::ReadingLevelSerializer
+
+  def date
+    object.created_at.strftime("%a %b %Y")
+  end
+
 end
